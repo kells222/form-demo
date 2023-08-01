43b3f0fc16a8eb61d077e803f2b1bf00cc2bc13e
Kelly Buckley <princesskells@Kellys-MacBook-Air.local>
2023-07-31 18:14:53 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,69 @@
-import logo from './logo.svg';
+
+
 import './App.css';
+import {useState} from "react"
 
 function App() {
+
+  const [FirstName,setFirstName]=useState("")
+  const [LastName,setLastName]=useState("")
+  const [Email,setEmail]=useState("")
+  const [Password,setPassword]=useState("")
+
+  function handleSubmit(e){
+    e.preventDefault()
+    console.log({FirstName,LastName,Email,Password})
+  }
+
+
+  function handleFirstNameChange(e){
+    setFirstName(e.target.value)
+  }
+
+    function handleLastNameChange(e){
+    setLastName(e.target.value)
+  }
+
+    function handleEmailChange(e){
+    setEmail(e.target.value)
+  }
+
+    function handlePasswordChange(e){
+    setPassword(e.target.value)
+  }
+
+// console.log({FirstName,LastName,Email,Password})
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <form onSubmit={handleSubmit}>
+      <label>
+      FirstName:
+      <input onChange={handleFirstNameChange} value={FirstName}  type='text'/>
+      </label>
+      <button type='submit'>submit</button>
+
+      <label>
+      LastName:
+      <input onChange={handleLastNameChange} value={LastName}  type='text'/>
+      </label>
+      <button type='submit'>submit</button>
+
+      <label>
+      Email:
+      <input onChange={handleEmailChange} value={Email}  type='text'/>
+      </label>
+      <button type='submit'>submit</button>
+
+      <label>
+      Password:
+      <input onChange={handlePasswordChange} value={Password} type='text'/>
+      </label>
+      <button type='submit'>submit</button>
+    </form>
+
+
     </div>
   );
 }
